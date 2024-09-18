4555e00b43a1be8ae52be32d16533176c686a522
martinleib <m@martinleib.uy>
2024-09-18 18:19:03 -0300

    Agregar archivos de proyecto.

---
+++ InterfazUsuario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InterfazUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LogicaNegocio/Sistema.cs
@@ -0,0 +1,7 @@
+﻿namespace LogicaNegocio
+{
+    public class Sistema
+    {
+
+    }
+}
