3073cc2d83ce94f9b990ea2aaa29650157857b66
martinleib <m@martinleib.uy>
2024-10-08 08:40:40 -0300

    Cliente.cs comentado

--- LogicaNegocio/Cliente.cs
+++ LogicaNegocio/Cliente.cs
@@ -9,20 +9,19 @@ namespace LogicaNegocio
     public class Cliente:Usuario
     {
         // Cliente hereda de usuario
-
+        
         // Saldo disponible
         private double _saldo;
 
-        public double Saldo
-        {
-            get { return _saldo; }
-        }
-
         public Cliente(string nombre, string apellido, string email, string password, double saldo) : base(nombre, apellido, email, password)
         {
+            // Es el método constructor de la clase cliente que se usará para crear instancias del tipo cliente.
+            // Al igual que en la clase Administrador se usa el constructor de la clase de la cual se está heredando (usuario) para construir la clase heredada
+            // En este caso, además, se construye la instancia de Cliente con un atríbuto extra (saldo).
             this._saldo = saldo;
         }
 
+        // Metodo que modifica el comportamiento del método ToString de la clase usuario, permitiendo imprimir los atributos nombre, apellido, email, ID y saldo.
         public override string ToString()
         {
             return $"Nombre completo: {Nombre} {Apellido}, informacion de contacto: {Email}, ID: {Id}, Saldo: {_saldo}";
