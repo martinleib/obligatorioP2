c4c690eacf9957980d7c75cdba23cd2ee4aa78d0
martinleib <m@martinleib.uy>
2024-10-08 08:47:30 -0300

    Subasta.cs comentado

--- LogicaNegocio/Subasta.cs
+++ LogicaNegocio/Subasta.cs
@@ -11,16 +11,19 @@ namespace LogicaNegocio
     {
         private List<Oferta>_ofertas = new List<Oferta>();
 
+        // Propiedad que devuelve la lista de ofertas de una instancia del tipo subasta.
         public List<Oferta> Ofertas
         {
             get { return _ofertas; }
         }
 
+        // Es el método constructor de la clase subasta que se usará para crear instancias del tipo subasta.
         public Subasta(string nombre, string estado, DateTime fechaPublicacion, Cliente comprador, Usuario finalizador, List<Articulo> articulos, List<Oferta> ofertas) : base(nombre, estado, fechaPublicacion, comprador, finalizador, articulos)
         {
             this._ofertas = ofertas;
         }
 
+        // Verifica si una oferta está en la lista de ofertas, devuelve true en caso de que este o false en caso de que no.
         public bool EstaEnOferta(Oferta oferta)
         {
             bool esta = false;
